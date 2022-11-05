3cb9dda1693674f242731649b9cf613b53fabdbd
fadlanrr26 <ramadanfadlan398@gmail.com>
2022-11-05 07:56:21 +0700

    OOP_DEYA_FADLAN_1202210226

---
+++ MODUL1/modul.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class modul {
+    public static void main (string[] args){
+        scanner sc = new scanner (system.in);
+        system.out.println ("selamat datang di restoran EAD");
+        system.out.println ('')
+    }
+}
