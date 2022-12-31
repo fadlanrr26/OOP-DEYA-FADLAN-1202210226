cc34b9db2a888f25e11deb183b19430bbe93193f
fadlanrr26 <116355741+fadlanrr26@users.noreply.github.com>
2022-12-31 07:41:34 +0700

    Add files via upload

---
+++ MODUL3_FADLAN/src/MODUL3_FADLAN/Main.java
@@ -0,0 +1,10 @@
+package MODUL3_FADLAN;
+import java.util.Scanner ;
+import java.util.MismatchException;
+
+public class main {
+    public static void main (String[]args){
+        calculation = new calculation();
+
+    }
+}
---
+++ MODUL3_FADLAN/src/MODUL3_FADLAN/calculation.java
@@ -0,0 +1,20 @@
+package MODUL3_FADLAN;
+
+public class calculation implements Runnable {
+    double radius;
+    double side ;
+    double area ;
+    double phi = 3.14 ;
+
+    public void setSquare(side,side){
+        double luas = (side*side) ;
+        while (true) {
+            try {
+
+            }
+        }
+    } 
+    public void getSquare(){
+
+    }
+}
